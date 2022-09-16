07dd005883831a2da19fe6ca0f2728f88f19554b
nswobodaSHA <113388294+nswobodaSHA@users.noreply.github.com>
2022-09-16 13:28:24 -0700

    fixed the snake body

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     vx = 0
-    vy = -1
+    vy = -1.5
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    vx = -1
+    vx = -1.5
     vy = 0
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    vx = 1
+    vx = 1.5
     vy = 0
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     vx = 0
-    vy = 1
+    vy = 1.5
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     if (atefoodflag == 0) {
@@ -23,18 +23,18 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSpr
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
-            . . . . . . 3 3 3 3 3 3 3 3 3 3 
-            . . . . . . 3 3 3 3 3 3 3 3 3 3 
-            . . . . . . 3 3 3 3 3 3 3 3 3 3 
-            . . . . . . 3 3 3 3 3 3 3 3 3 3 
-            . . . . . . 3 3 3 3 3 3 3 3 3 3 
-            . . . . . . 3 3 3 3 3 3 3 3 3 3 
-            . . . . . . 3 3 3 3 3 3 3 3 3 3 
-            . . . . . . 3 3 3 3 3 3 3 3 3 3 
-            . . . . . . 3 3 3 3 3 3 3 3 3 3 
-            . . . . . . 3 3 3 3 3 3 3 3 3 3 
+            . . . . . . f f 7 7 7 7 7 7 f f 
+            . . . . . . 7 7 f f f f f f 7 7 
+            . . . . . . f f 7 7 7 7 7 7 f f 
+            . . . . . . 7 7 f f f f f f 7 7 
+            . . . . . . f f 7 7 7 7 7 7 f f 
+            . . . . . . 7 7 f f f f f f 7 7 
+            . . . . . . f f 7 7 7 7 7 7 f f 
+            . . . . . . 7 7 f f f f f f 7 7 
+            . . . . . . f f 7 7 7 7 7 7 f f 
+            . . . . . . 7 7 f f f f f f 7 7 
             `, SpriteKind.Player))
-        snakebody[0].setPosition(foodx - updatevx * 10, foody - updatevy * 10)
+        snakebody[0].setPosition(foodx - updatevx * 10 + 2, foody - updatevy * 10 + 2)
         foodx = randint(0, 15) * 10
         foody = randint(0, 11) * 10
     }
@@ -57,16 +57,16 @@ let snakehead = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    . . . . . . 3 3 3 3 3 3 3 3 3 3 
-    . . . . . . 3 3 3 3 3 3 3 3 3 3 
-    . . . . . . 3 3 3 3 3 3 3 3 3 3 
-    . . . . . . 3 3 3 3 3 3 3 3 3 3 
-    . . . . . . 3 3 3 3 3 3 3 3 3 3 
-    . . . . . . 3 3 3 3 3 3 3 3 3 3 
-    . . . . . . 3 3 3 3 3 3 3 3 3 3 
-    . . . . . . 3 3 3 3 3 3 3 3 3 3 
-    . . . . . . 3 3 3 3 3 3 3 3 3 3 
-    . . . . . . 3 3 3 3 3 3 3 3 3 3 
+    . . . . . . 7 7 7 7 7 7 7 7 7 7 
+    . . . . . . 7 7 7 7 7 7 7 7 7 7 
+    . . . . . . 7 7 7 7 7 7 7 7 7 7 
+    . . . . . . 7 7 f 7 7 7 7 f 7 7 
+    . . . . . . 7 7 7 7 7 7 7 7 7 7 
+    . . . . . . 7 7 7 7 7 7 7 7 7 7 
+    . . . . . . 7 7 f f f f f f 7 7 
+    . . . . . . 7 f 3 1 3 3 1 3 f 7 
+    . . . . . . 7 f 3 3 2 2 3 3 f 7 
+    . . . . . . 7 7 f f 2 2 f f 7 7 
     `, SpriteKind.Player)
 let snakefood = sprites.create(img`
     . . . . . . . . . . . . . . . . 
@@ -75,16 +75,16 @@ let snakefood = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    . . . . . . 2 2 2 2 2 2 2 2 2 2 
-    . . . . . . 2 2 2 2 2 2 2 2 2 2 
-    . . . . . . 2 2 2 2 2 2 2 2 2 2 
-    . . . . . . 2 2 2 2 2 2 2 2 2 2 
-    . . . . . . 2 2 2 2 2 2 2 2 2 2 
-    . . . . . . 2 2 2 2 2 2 2 2 2 2 
-    . . . . . . 2 2 2 2 2 2 2 2 2 2 
-    . . . . . . 2 2 2 2 2 2 2 2 2 2 
-    . . . . . . 2 2 2 2 2 2 2 2 2 2 
-    . . . . . . 2 2 2 2 2 2 2 2 2 2 
+    . . . . . . f f f f 7 7 f f f f 
+    . . . . . . f f f f 7 f f f f f 
+    . . . . . . f f f 2 2 2 2 f f f 
+    . . . . . . f f 2 2 2 2 2 2 f f 
+    . . . . . . f 2 2 2 2 2 2 2 2 f 
+    . . . . . . f 2 2 2 2 2 2 2 2 f 
+    . . . . . . f 2 2 2 2 2 2 2 2 f 
+    . . . . . . f 2 2 2 2 2 2 2 2 f 
+    . . . . . . f f 2 2 2 2 2 2 f f 
+    . . . . . . f f f 2 2 2 2 f f f 
     `, SpriteKind.Food)
 let snakeheadx = 0
 let snakeheady = 0
@@ -103,19 +103,19 @@ game.onUpdate(function () {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
-            . . . . . . 3 3 3 3 3 3 3 3 3 3 
-            . . . . . . 3 3 3 3 3 3 3 3 3 3 
-            . . . . . . 3 3 3 3 3 3 3 3 3 3 
-            . . . . . . 3 3 3 3 3 3 3 3 3 3 
-            . . . . . . 3 3 3 3 3 3 3 3 3 3 
-            . . . . . . 3 3 3 3 3 3 3 3 3 3 
-            . . . . . . 3 3 3 3 3 3 3 3 3 3 
-            . . . . . . 3 3 3 3 3 3 3 3 3 3 
-            . . . . . . 3 3 3 3 3 3 3 3 3 3 
-            . . . . . . 3 3 3 3 3 3 3 3 3 3 
+            . . . . . . f f 7 7 7 7 7 7 f f 
+            . . . . . . 7 7 f f f f f f 7 7 
+            . . . . . . f f 7 7 7 7 7 7 f f 
+            . . . . . . 7 7 f f f f f f 7 7 
+            . . . . . . f f 7 7 7 7 7 7 f f 
+            . . . . . . 7 7 f f f f f f 7 7 
+            . . . . . . f f 7 7 7 7 7 7 f f 
+            . . . . . . 7 7 f f f f f f 7 7 
+            . . . . . . f f 7 7 7 7 7 7 f f 
+            . . . . . . 7 7 f f f f f f 7 7 
             `, SpriteKind.Player))
-        snakebody[0].setPosition(snakeheadx, snakeheady)
-        snakebody.pop()
+        snakebody[0].setPosition(snakeheadx + 2, snakeheady + 2)
+        snakebody.pop().destroy()
         updatevx = vx
         updatevy = vy
         atefoodflag = 0
